6fe5da8f022e73deba66ad3b5d5cd7abd55900da
seojin <seojinyoon4@gmail.com>
2021-03-22 10:29:58 +0900

    영상 0205

--- CLMN/Replay_Exp/main(revise).py
+++ CLMN/Replay_Exp/main(revise).py
@@ -1,3 +1,21 @@
+
+"""
+목표: sequence만 부호화하는 것
+
+제거해야 할 사항
+숫자 하나만 입력하는 것 다 없애버려야함
+
+수정해야 할 사항
+- 시퀀스가 학습되었는지에 대한 보상 - 별로 표현(오른쪽 위)
+- 맞으면 시퀀스가 변경되어 *로 표시됨
+- 버튼 입력 들어오면 포즈 같은거 해야함(누르고 있으면 계속 들어옴)
+- 런 횟수 8개
+
+수정 blue print
+
+"""
+
+
 """
 Library Settings
 """
@@ -19,27 +37,6 @@ from Higher_function.sj_higher_function import recursive_map
 Data Setting
 """
 # making units
-
-unit_data = ["1", "2", "3", "4"]
-list_units = list(itertools.permutations(unit_data, 4))
-shuffle_list(list_units)
-
-units = recursive_map(list_units, lambda x: Text_st_unit(x, showing_time=0.5))
-
-ISIs = []
-ISI_file_path = os.path.join(source_path, "ISI.txt")
-with open(ISI_file_path, "r") as f:
-    ISIs = f.readlines()
-    ISIs = list(map(lambda x: int(x), ISIs))
-
-u_sets = []
-for i in range(0, len(units)):
-    j = i * 3
-
-    u_s = units[i]
-    u_sets.append(Text_st_bundle(units=u_s,
-                                 ISI_times=[ISIs[j], ISIs[j+1], ISIs[j+2]]))
-
 # Making sequences
 seq1 = Sequence_st_bundle([Sequence_st_text_unit(["4", "3", "2", "1", "4"], showing_time=15, color=[0,0,0], text_height=0.1)],
                           ISI_interval= 21)
@@ -51,55 +48,6 @@ seq_set_list = list(map(lambda x: [seq2, seq1] if x == 1 else [seq1, seq2], samp
 
 shuffle_list(seq_set_list)
 
-# combine unit and sequence
-def make_blocks(u_sets, seq_set_list):
-    combination_units = []
-
-    seq_list = []
-    for seq_set in seq_set_list:
-        seq_list.append(seq_set[0])
-        seq_list.append(seq_set[1])
-
-    sequence_length = len(seq_list)
-    unit_set_length = len(u_sets)
-
-    time_interval_between_bundles = [10, 0]
-
-    blocks = []
-    if unit_set_length > sequence_length and (unit_set_length % sequence_length) == 0:
-        for i in range(0, sequence_length):
-            combination_unit = (u_sets[i], seq_list[i])
-            combination_units.append(combination_unit)
-
-        datas = list(map(lambda x: St_Pakcage(bundles=[x[0], x[1]],
-                                      bundle_intervals=time_interval_between_bundles,
-                                      interval_text="..."), combination_units))
-
-        # datas.append(Text_st_unit("End session", showing_time=10))
-        blocks.append(datas)
-
-        # Sequence is mached all, so they need to be shuffled
-        shuffle_list(seq_list)
-
-        combination_units = []
-        for i in range(0, len(seq_list)):
-            combination_unit = (u_sets[i+sequence_length], seq_list[i])
-            combination_units.append(combination_unit)
-
-        datas = list(map(lambda x: St_Pakcage(bundles=[x[0], x[1]],
-                                                    bundle_intervals=time_interval_between_bundles,
-                                                    interval_text="..."), combination_units))
-        # datas.append(Text_st_unit("End session", showing_time=10))
-        blocks.append(datas)
-
-        # u_sets and sequence are mached all, so they need to be shuffled
-        shuffle_list(u_sets)
-        shuffle_list(seq_list)
-    return blocks
-
-blocks = []
-for t in range(0,4):
-    blocks += make_blocks(u_sets, seq_set_list)
 
 # 나중에 사용할때 이것만 고쳐주면 됨
 
