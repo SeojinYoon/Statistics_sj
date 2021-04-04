9f8003fc9e8fa1d8f17684a60772aad9ac3a84c8
seojin <seojinyoon4@gmail.com>
2021-04-04 13:00:50 +0900

    clean

--- playground.py
+++ playground.py
@@ -1,22 +1,4 @@
 
 
-print(2)
 
-print(30)
 
-import numpy as np
-
-# one-to-many mapping
-def one_to_many(X, Y):
-    """
-
-    """
-
-for y in [1,2,3]:
-
-X = [1,2,3]
-map(lambda x: np.repeat(x, 3), X)
-
-# many-to-one mapping
-
-# many-to-many mappign
