4c2d7d109d14a0b606bd66188aaac931e6e5eff0
seojin <seojinyoon4@gmail.com>
2021-04-04 12:59:59 +0900

    make mapping function

--- Higher_function/sj_higher_function.py
+++ Higher_function/sj_higher_function.py
@@ -65,7 +65,89 @@ def apply_function(f, args):
         raise Exception("the number of function's parameter is not matched args")
 
 def flatten_2d(a_2dlist):
+    """
+    flatten list 2d -> 1d
+
+    :param a_2dlist: 2d list
+    :return: 1d list
+    """
     return list(itertools.chain(*a_2dlist))
+
+def flatten(l):
+    try:
+        return flatten(l[0]) + (flatten(l[1:]) if len(l) > 1 else []) if type(l) is list else [l]
+    except IndexError:
+        return []
+
+class Mapping:
+    """
+    This class is for defining mapping function
+
+    This class is used for namespace
+    """
+
+    @staticmethod
+    def one_to_many(domain, codomain, mapping_condition):
+        """
+
+        :param domain: domain
+        :type domain: list
+        :param codomain: codomain
+        :type codomain: list
+        :param mapping_condition: mapping condition (argument count is 2)
+        :type mapping_condition: function
+
+        :return: mapped list(domain to codomain)
+        :rtype: 2d list
+        """
+        return list(map(lambda x: Mapping.one_value_to_many(x, list(filter(lambda y: mapping_condition(x, y), codomain))), domain))
+
+    @staticmethod
+    def one_value_to_many(x, Y):
+        """
+
+        :param x: a value
+        :type x: scalar
+        :param Y: codomain
+        :type Y: list
+        :return: [[x, y1], [x, y2], ...]
+        :rtype: list
+        """
+        return [[x, y] for y in Y]
+
+    @staticmethod
+    def one_to_one(X, Y, condition):
+        """
+        one_to_one mapping
+        if one value is selected then the value is not mapped any more(successively)
+        :param X:
+        :type X: list
+        :param Y:
+        :type Y: list
+        :param condition: mapping condition
+        :type condition: (x, y) -> Flag
+        :return: mapped values
+        :rtype: 2d list
+        """
+        default_value_format = "None {0}"
+        default_count = 0
+
+        target_Y = Y[:]
+        result = []
+        for x in X:
+            is_mapping_occurred = False
+            for y in target_Y:
+                if condition(x,y):
+                    result.append([x, y])
+                    target_Y.remove(y) # remove first occurred value y
+                    is_mapping_occurred = True
+                    break
+
+            if is_mapping_occurred == False:
+                defalut_value = str.format(default_value_format, str(default_count))
+                result.append([x, defalut_value])
+                default_count += 1
+        return result
 
 if __name__ == "__main__":
     apply_function(lambda x,a,b: x+a+b, [1,2,3])
@@ -77,3 +159,6 @@ if __name__ == "__main__":
     apply_composition_function([1,2,3], [lambda x: x*2, lambda x: x**2])
 
     flatten_2d([[1,2], [3,4]])
+
+    Mapping.one_to_many([1, 2, 3], [4, 5, 6], lambda x, y: y > x)
+    Mapping.one_to_one([1, 2, 3], [4, 5, 6], lambda x, y: (x + y) > 7)
--- playground.py
+++ playground.py
@@ -1,27 +1,22 @@
 
-from File_Package.sj_file_system import CsvManager
-import os
 
-csv_m = CsvManager(os.getcwd(), "ANIMAL_INS")
-csv_m.write_header(["ANIMAL_ID", "ANIMAL_TYPE", "DATETIME"])
-csv_m.write_row([1, "Dog", '2020-10-14 15:38:00'])
-csv_m.write_row([2, "Dog", '2020-10-23 11:42:00'])
-csv_m.write_row([3, "Dog",'2020-11-03 15:04:00'])
-csv_m.write_row([4, "Dog", '2020-11-18 17:03:00'])
-csv_m.write_row([5, "Iguana", '2021-01-18 13:00:00'])
-csv_m.write_row([6, "Snake", '2021-03-18 12:00:00'])
-csv_m.write_row([7, "Dog", '2021-03-18 09:03:00'])
-csv_m.write_row([8, "Iguana", '2021-03-31 09:00:00'])
-csv_m.write_row([9, "Dog", '2021-03-21 09:00:00'])
+print(2)
 
-csv_m = CsvManager(os.getcwd(), "ANIMAL_INFO")
-csv_m.write_header(["ANIMAL_ID", "INTAKE_CONDITION", "ANIMAL_NAME", "SEX_UPON_INTAKE"])
-csv_m.write_row([1, 'Normal', 'Jack', 'Neutered Male'])
-csv_m.write_row([2, 'Normal', 'Disciple', 'Intact Male'])
-csv_m.write_row([3, 'Normal', 'Katie', 'Spayed Female'])
-csv_m.write_row([4, 'Normal', 'Anna', 'Spayed Female'])
-csv_m.write_row([5, 'Wounded', 'Cherry', 'Intact Male'])
-csv_m.write_row([6, 'Normal', 'Abo', 'Intact FeMale'])
-csv_m.write_row([7, 'Normal', 'Perry', 'Intact FeMale'])
-csv_m.write_row([8, 'Wounded', 'Uri', 'Intact FeMale'])
-csv_m.write_row([9, 'Normal', 'Sun', 'Neutered Male'])
+print(30)
+
+import numpy as np
+
+# one-to-many mapping
+def one_to_many(X, Y):
+    """
+
+    """
+
+for y in [1,2,3]:
+
+X = [1,2,3]
+map(lambda x: np.repeat(x, 3), X)
+
+# many-to-one mapping
+
+# many-to-many mappign
